34a1bdda9ae058292198c9395febdc80566b6af0
Garrett Breaux <garrett.breaux1@gmail.com>
2021-08-03 18:00:29 -0500

    Practice accomplished

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> games = new List<string>() {"Call of Duty", "Jump Force", "Dragonballz", "NBA21", "Destiny" };
+           var sortedGames = games.Where(name=> name.Length >1).OrderBy(name =>name.Length);
+          
+            foreach(var title in sortedGames)
+            {
+                Console.WriteLine($"{title}");
+            }
+
+        }
+    }
+}
